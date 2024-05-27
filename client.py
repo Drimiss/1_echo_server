870fcd48b6b8456df1b61b76a6fcbcc04aef57b8
Ksu <brooksdi@student.21-school.ru>
2024-05-27 16:14:08 +0300

    all ready

--- client.py
+++ client.py
@@ -1,16 +1,29 @@
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+IP = input('Введите ip адрес для подключения(значение по умолчанию - 127.0.0.1): ')
+if IP == '':
+    IP = '127.0.0.1'
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+port = input('Введите порт для подключения(значение по умолчанию - 12345): ')
+if port == '':
+    port = 12345
 
-data = sock.recv(1024)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, port))
+print(f'Соединение Успешно!')
+try:
+    while True:
+        message_from_server = client_socket.recv(1024).decode()
+        print(f'Получено от сервера: {message_from_server}')
 
-sock.close()
+        message_from_client = input('Введите текст для отправки: ')
 
-print(data.decode())
+        client_socket.sendall(message_from_client.encode())
+        print(f'Отправлено: {message_from_client}')
+
+        if message_from_client == 'exit':
+            break
+except:
+    print('ОЙ,что-то пошло не так, попробуйте снова(')
+    client_socket.close()
+client_socket.close()
